80568b757726f4a9ed2e9e326ca7b33a2ad65c53
kodiitulip <kodii.tulip@proton.me>
2026-08-24 22:37:42 -0300

    starters

---
+++ src/assets/svg/boberto/index.ts
@@ -0,0 +1,6 @@
+export interface Props extends astroHTML.JSX.SVGAttributes {
+  size?: number;
+}
+
+export { default as BobertoBright } from "./bright.astro";
+export { default as BobertoDark } from "./dark.astro";
