e6e596dbea51da70f591061fabee4bef86204e08
kodiitulip <kodii.tulip@proton.me>
2026-09-04 14:38:09 -0300

    make boberto svgs be square + use color css variables with fallback

--- src/assets/svg/boberto/index.ts
+++ src/assets/svg/boberto/index.ts
@@ -2,5 +2,8 @@ export interface Props extends astroHTML.JSX.SVGAttributes {
   size?: number;
 }
 
+export const colorDark = "var(--color-dark-950,#1c1c1c)";
+export const colorBright = "var(--color-bright-50,#fffcf7)";
+
 export { default as BobertoBright } from "./bright.astro";
 export { default as BobertoDark } from "./dark.astro";
