d4dbeca577044525ad526b73d884057ee3104d68
kodiitulip <kodii.tulip@proton.me>
2026-09-04 16:30:02 -0300

    holy shit links are so anoying

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ca-smd.github.io/mediaweek",
-  base: "/mediaweek/",
+  base: "/mediaweek",
 
   vite: {
     plugins: [tailwindcss()],
