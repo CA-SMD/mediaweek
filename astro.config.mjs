41bf4ecf40bc1e17a07c826fe003c60993f6aad6
kodiitulip <kodii.tulip@proton.me>
2026-09-07 15:52:36 -0300

    finnaly i fixed the link stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ca-smd.github.io/mediaweek",
+  site: "https://ca-smd.github.io/",
   base: "/mediaweek/",
 
   vite: {
