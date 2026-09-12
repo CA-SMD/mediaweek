89d23252e61c1c91ad5460a5fd6475dab4dde9cf
kodiitulip <kodii.tulip@proton.me>
2026-09-12 19:32:20 -0300

    formmating

--- src/assets/svg/boberto/index.ts
+++ src/assets/svg/boberto/index.ts
@@ -2,16 +2,18 @@ export interface Props extends astroHTML.JSX.SVGAttributes {
   size?: number;
 }
 
-export const colorDark = 'var(--color-dark-950,#1C1C1C)';
-export const colorBright = 'var(--color-bright-50,#FFFCF7)';
-export const colorJogos = 'var(--color-jogos-500,#FF5343)';
-export const colorAudiovisual = 'var(--color-audiovisual-500,#00DD90)';
-export const colorDesign = 'var(--color-design-500,#FFA2FF)';
-export const colorSistemas = 'var(--color-sistemas-500,#01A0EA)';
+export type BobertoIcon = (_props: Props) => unknown;
 
-export { default as BobertoBright } from './bright.astro';
-export { default as BobertoDark } from './dark.astro';
-export { default as BobertoJogos } from './jogos.astro';
-export { default as BobertoSistemas } from './sistemas.astro';
-export { default as BobertoDesign } from './design.astro';
-export { default as BobertoAudiovisual } from './audiovisual.astro';
+export const colorDark = "var(--color-dark-950,#1C1C1C)";
+export const colorBright = "var(--color-bright-50,#FFFCF7)";
+export const colorJogos = "var(--color-jogos-500,#FF5343)";
+export const colorAudiovisual = "var(--color-audiovisual-500,#00DD90)";
+export const colorDesign = "var(--color-design-500,#FFA2FF)";
+export const colorSistemas = "var(--color-sistemas-500,#01A0EA)";
+
+export { default as BobertoBright } from "./bright.astro";
+export { default as BobertoDark } from "./dark.astro";
+export { default as BobertoJogos } from "./jogos.astro";
+export { default as BobertoSistemas } from "./sistemas.astro";
+export { default as BobertoDesign } from "./design.astro";
+export { default as BobertoAudiovisual } from "./audiovisual.astro";
